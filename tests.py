20ab50bbdb86d72f34874f6a7b1d9a3ccee78788
LachubCz <petr.genius.buchal@gmail.com>
2018-05-20 10:43:03 +0200

    new requirments

--- tests.py
+++ tests.py
@@ -7,11 +7,11 @@ import subprocess
 class TestAppRuns(unittest.TestCase):
 
     def test_output_check(self):
+        os.system("python3 pgunn/main.py -mode test -env 2048-v0 -eps 25 -mdl models/2048-v0_basic.h5")
+        os.system("python3 pgunn/main.py -mode test -env Breakout-ram-v0 -eps 10 -mdl models/Breakout-ram-v0_basic.h5")
         output = subprocess.check_output("python3 pgunn/main.py -mode train -env CartPole-v0 -eps 2", shell=True)
         rgx = re.compile(r'''\[Graph\sof\slearning\sprogress\svisualization\swas\smade\.\]''', re.X)
         self.assertTrue(rgx.search(str(output)))
 
 if __name__ == "__main__":
     unittest.main()
-    os.system("python3 pgunn/main.py -mode test -env 2048-v0 -eps 25 -mdl models/2048-v0_basic.h5")
-    os.system("python3 pgunn/main.py -mode test -env Breakout-ram-v0 -eps 10 -mdl models/Breakout-ram-v0_basic.h5")
