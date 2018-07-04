5451492136478b83cd300a15dc1f04bed1e2dfbe
LachubCz <petr.genius.buchal@gmail.com>
2018-07-04 10:48:57 +0200

    deletion of nonimportant files

--- pgunn/web.py
+++
@@ -1,9 +0,0 @@
-import os
-import datetime
-import time
-
-while True:
-    os.system("ssh xbucha02@merlin.fit.vutbr.cz '(cd WWW ; python3.6 get_data.py)' > results.out")
-    now = datetime.datetime.now()
-    print(now.strftime("%Y-%m-%d %H:%M"))
-    time.sleep(300)
