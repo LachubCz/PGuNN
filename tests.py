6d5bd49668620545ce5b60d17f9541643e97d942
LachubCz <petr.genius.buchal@gmail.com>
2019-02-22 12:40:11 +0100

    More tests added.

--- tests.py
+++ tests.py
@@ -3,21 +3,122 @@ import unittest
 import subprocess
 
 class TestAppRuns(unittest.TestCase):
-    
-    def test_cct_env(self):
-        output = subprocess.check_output("python3 pgunn/main.py -mode train -env CartPole-v0 -eps 2", shell=True)
+    def train_cct_env_01(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env CartPole-v1 -eps 2 -init -net dueling -vids", shell=True)
         rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
         self.assertTrue(rgx.search(str(output)))
 
 
-    def test_atari_env(self):
-        output = subprocess.check_output("python3 pgunn/main.py -mode train -env Breakout-ram-v0 -eps 2", shell=True)
+    def train_ram_atari_env_01(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env BeamRider-ram-v0 -eps 2 -init -net dueling", shell=True)
         rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
         self.assertTrue(rgx.search(str(output)))
 
 
-    def test_2048_env(self):
-        output = subprocess.check_output("python3 pgunn/main.py -mode train -env 2048-v0 -eps 2", shell=True)
+    def train_img_atari_env_01(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env BeamRider-v0 -frames 3 -eps 2 -init -net dueling", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def train_2048_env_01(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env 2048-v0 -eps 2 -init -net dueling", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def train_cct_env_02(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env CartPole-v0 -eps 2 -init -mem prioritized -vids", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def train_ram_atari_env_02(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env Breakout-ram-v0 -eps 2 -init -mem prioritized", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def train_img_atari_env_02(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env Breakout-v0 -frames 3 -eps 2 -init -mem prioritized", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def train_2048_env_02(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env 2048-v0 -eps 2 -init -mem prioritized", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def train_cct_env_03(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env MountainCar-v0 -eps 2 -alg DQN+TN -update_f 1", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def train_ram_atari_env_03(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env SpaceInvaders-ram-v0 -eps 2 -init -alg DQN+TN", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def train_img_atari_env_03(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env SpaceInvaders-v0 -frames 4 -eps 2 -init -alg DQN+TN", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def train_2048_env_03(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env 2048-v0 -eps 2 -init -mem prioritized -alg DQN+TN -update_f 1", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def train_cct_env_04(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env Acrobot-v1 -eps 2 -alg DDQN -save_f 1", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def train_ram_atari_env_04(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env Breakout-ram-v0 -eps 2 -init -alg DDQN -mdl_blueprint -save_f 2", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def train_img_atari_env_04(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env Breakout-v0 -frames 2 -eps 2 -init -alg DDQN -mdl_blueprint -save_f 2", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def train_2048_env_04(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode train -env 2048-v0 -eps 2 -init -mem prioritized -alg DDQN -dont_save", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def test_cct_env_05(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode test -env CartPole-v1 -eps 2 -mdl ./models/CartPole-v1_basic.h5", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def test_ram_atari_env_05(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode test -env BeamRider-ram-v0 -eps 2 -mdl ./models/BeamRider-ram-v0_basic.h5", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def test_img_atari_env_05(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode test -env BeamRider-v0 -frames 2 -eps 2 -mdl ./models/BeamRider-v0_basic.h5", shell=True)
+        rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
+        self.assertTrue(rgx.search(str(output)))
+
+
+    def test_2048_env_05(self):
+        output = subprocess.check_output("python3 pgunn/main.py -mode test -env 2048-v0 -eps 2 -mdl ./models/2048-v0_basic.h5", shell=True)
         rgx = re.compile(r'''\[SUCCESSFUL\sRUN\]''', re.X)
         self.assertTrue(rgx.search(str(output)))
 
